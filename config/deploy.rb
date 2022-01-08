7aab8a433f5e6408d6b27cc611e7ed18466a87ca
Hazel <xp0729@gmail.com>
2022-01-08 11:20:28 +0800

    add cap

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "coin_task"
+set :repo_url, "git@github.com:brtr/coin_task.git"
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/app/coin_task"
+
+set :linked_files, fetch(:linked_files, []).push('config/application.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :puma_config_path, -> { File.join(current_path, "config", "puma_production.rb") }
+
+set :pty, false
+
+set :rails_env, fetch(:staging)
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '3.0.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :bundler_path, "/home/deploy/.rbenv/shims/bundle"
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
